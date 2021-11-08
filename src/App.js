366a1cc5434ae251f9ccda3bb2da7f68bde47ac1
Weronika Kansy <weronika.kansy@gmail.com>
2021-11-08 23:38:12 +0100

    Track and Player components created and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./styles/app.scss";
+import Player from "./components/Player";
+import Track from "./components/Track";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Track />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Track.js
@@ -0,0 +1,11 @@
+const Track = () => {
+  return (
+    <div className="track-container">
+      <h2>Picture</h2>
+      <h2>Track Name</h2>
+      <h2>Artist</h2>
+    </div>
+  );
+};
+
+export default Track;
