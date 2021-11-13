e858e946d61834cabc9d642e3cc7cd0a49b3c687
Weronika Kansy <weronika.kansy@gmail.com>
2021-11-13 01:41:54 +0100

    Audio data stored in a state inside App with the useState hook; another state added for the current track data and passed to the Track component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import "./styles/app.scss";
 import data from "./data";
 import Player from "./components/Player";
 import Track from "./components/Track";
 
 function App() {
+  const [tracks, setTracks] = useState(data());
+  const [currentTrack, setcurrentTrack] = useState(tracks[0]);
+
   return (
     <div className="App">
-      <Track />
+      <Track currentTrack={currentTrack} />
       <Player />
     </div>
   );
