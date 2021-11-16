04e8d8853d6e7832d1b44feb88cf9ba6b192eec8
Weronika Kansy <weronika.kansy@gmail.com>
2021-11-17 00:26:14 +0100

    Headings inside LibraryItem wrapped in a div; library styles defined in a new partial; Google font link updated

--- src/components/LibraryItem.js
+++ src/components/LibraryItem.js
@@ -4,8 +4,10 @@ const LibraryItem = ({ track }) => {
       <div className="library-item__img-container">
         <img src={track.cover} alt={track.title} />
       </div>
-      <h3>{track.title}</h3>
-      <h4>{track.artist}</h4>
+      <div className="library-item__description">
+        <h3>{track.title}</h3>
+        <h4>{track.artist}</h4>
+      </div>
     </div>
   );
 };
